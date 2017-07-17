53951523459eaf7906c78c6b89edb4028df9a5cd
itsianida <stndnurhidayah62@gmail.com>
2017-07-17 15:11:35 +0700

    Add files via upload

---
+++ Pages/cuti.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 17, 2017 at 09:22 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cuti`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `nik` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `departemen` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `tipe` tinyint(1) NOT NULL,
+  `countcuti` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`nik`, `nama`, `username`, `email`, `password`, `departemen`, `status`, `tipe`, `countcuti`) VALUES
+('1', 'eno', 'eno', 'eno@gmail.com', 'eno123', 'hrd', 0, 0, 0),
+('2', 'nadia', 'nadia', 'nadia@gmail.com', 'nadia123', 'pemasaran', 0, 0, 0),
+('3', 'anggun', 'anggun', 'anggun@gmail.com', 'anggun123', 'pemasaran', 0, 1, 2),
+('4', 'nizar', 'nizar', 'nizar@gmail.com', 'nizar123', 'hrd', 0, 0, 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`nik`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
